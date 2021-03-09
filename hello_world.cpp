bce67b8072c8120bb986f3d30c2054f18028cca4
razuwaikin <artyomgorbatenko@gmail.com>
2021-03-09 13:04:39 +0300

    added comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
 int main(){
-string a; std::cin>>a;
+string a; std::cin>>a;//deleted"using namespace std"
 std::cout<<endl<<"HELLO WORLD FROM "<<a<<endl;
 return 1;}
