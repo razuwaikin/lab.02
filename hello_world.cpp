c043da74f1ee11d9affbb32a2990ccd25dbd64c0
razuwaikin <artyomgorbatenko@gmail.com>
2021-03-10 23:04:03 +0300

    reworked hw

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 <<<<<<< HEAD
+<<<<<<< HEAD
 int main(){//i like my style of code
 string a; std::cin>>a;//deleted"using namespace std"
 std::cout<<endl<<"HELLO WORLD, FROM "<<a<<" !"<<endl;//worked hard on this part
@@ -15,3 +16,9 @@ main()
   return 1;
 }
 >>>>>>> new style
+=======
+int main(){
+  string a;std::cin >> a; // deleted"using namespace std"
+  std::cout << endl << "HELLO WORLD FROM " << a << endl;
+  return 1;}
+>>>>>>> reworked hw
