8d6d34a357b1a5582a1bcb573addd2173a13753d
razuwaikin <artyomgorbatenko@gmail.com>
2021-03-10 23:24:54 +0300

    still workin on style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-int main(){
+int main(){//i like my style of code
 string a; std::cin>>a;//deleted"using namespace std"
-std::cout<<endl<<"HELLO WORLD FROM "<<a<<endl;
+std::cout<<endl<<"HELLO WORLD, FROM "<<a<<" !"<<endl;//worked hard on this part
 return 1;}
